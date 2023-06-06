d3ffa412b05b7a20dc168763b1d693a6eb83cdb8
amiltonlopes24 <57579600+amiltonlopes24@users.noreply.github.com>
2023-06-05 21:16:57 -0300

    Add files via upload

---
+++ JogoCartas.cs/JogoCartas.cs/Program - Cópia.cs
@@ -0,0 +1,16 @@
+﻿namespace classes
+{
+    public class Classes
+    {
+        public string Nome ()
+        {
+            Console.WriteLine("Digite um nome: ");
+            string Nome = Console.ReadLine();
+
+            return Nome + "\n" +
+            "Energias: 10";
+
+        }
+
+    }
+}
---
+++ JogoCartas.cs/JogoCartas.cs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using classes;
+
+
+namespace ProjetoJogo
+
+{
+    public class Jogo
+    {
+
+        public static void Main(string[] args)
+        {
+            string Opcao;
+            int vez = 1;
+            string[] cartas =  new string[3] { "Copas","Espada", "Ouros" };
+            ArrayList arr = new ArrayList();
+            do
+            {
+                Console.WriteLine("Olá, vamos jogar cartas ");
+                Console.WriteLine("Digite o nome do jogador " + vez);
+
+                Classes Jogador = new Classes();
+
+                var Jogador1 = Jogador.Nome();
+
+                arr.Add(Jogador1);
+                if (arr.Count == 2) break;
+                Console.WriteLine("Deseja Adicionar mais jogadores?");
+                
+                Opcao = Console.ReadLine().ToUpper();
+                
+
+                vez++;
+
+
+            } while (Opcao != "NAO");
+
+     
+
+            for (int i  = 0; i <= 3;  i++ )
+            {
+              Console.WriteLine ( new Random().Next(1, 3));
+               
+            }
+
+            if (arr.Count == 1)
+            {
+             
+            }
+            
+
+
+        }
+
+
+    }
+
+}
